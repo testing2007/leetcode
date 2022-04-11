58a3d8cd7d1a503be4083456bfb144d0bdd4daf9
ZhiQiang wei <xiaoqiang0134@163.com>
2022-04-11 18:37:21 +0800

    添加leetcode环境

---
+++ hashtable/1.two-sum.java
@@ -0,0 +1,40 @@
+package hashtable;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int j = 0; j < nums.length; j++) {
+      int key = target - nums[j];
+      if (map.containsKey(nums[j])) {
+        int[] ret = new int[2];
+        ret[0] = map.get(nums[j]);
+        ret[1] = j;
+        return ret;
+      }
+      map.put(key, j);
+    }
+    return null;
+  }
+
+}
+
+class Main {
+  public static void main(String[] args) {
+    Solution so = new Solution();
+    int[] nums = { -1, -2, -3, -4 };
+    int[] ret = so.twoSum(nums, -6);
+    if (ret!=null && ret.length == 2) {
+      System.out.println("ret[0]=" + ret[0] + ",ret[1]=" + ret[1]);
+    }
+  }
+}
+// @lc code=end
---
+++ hashtable/3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,23 @@
+package hashtable;
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+class Solution3 {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
+
+class Main3 {
+  public static void main(String[] args) {
+    Solution3 so = new Solution3();
+    int n = so.lengthOfLongestSubstring("abc");
+    System.out.println("result n="+n);
+  }
+}
+// @lc code=end
+
